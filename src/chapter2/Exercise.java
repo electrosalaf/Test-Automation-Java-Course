45129e2f5d329e8468d54bb86705623b4eb90b3f
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2022-06-02 13:25:09 +0100

    Initial Project

---
+++ src/chapter2/Exercise.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+
+        System.out.println("What season of the year are we?");
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.next();
+
+        System.out.println("What does the weather feels like? ");
+        String weather = scanner.next();
+
+        System.out.println("How many cup of coffee did you take?");
+        int numberOfCoffee = scanner.nextInt();
+
+        System.out.println("On a " + weather + " " + season +
+                " day, I drink a minimum of " + numberOfCoffee + " cups of coffee.");
+    }
+}
