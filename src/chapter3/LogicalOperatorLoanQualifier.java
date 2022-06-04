3ab460eb8c98cc30f1732cf61a1a2cddd5223edd
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2022-06-04 15:18:10 +0100

    updated project

--- src/chapter3/LogicalOperatorLoanQualifier.java
+++ src/chapter3/LogicalOperatorLoanQualifier.java
@@ -1,14 +1,32 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class LogicalOperatorLoanQualifier {
 
-    // Initiate what you know
-    int requiredSalary = 3000;
-    int requiredYearsEmployed = 2;
+    public static void main(String[] args) {
 
-    // Get the unknown
+        // Initiate what you know
+        int requiredSalary = 3000;
+        int requiredYearsEmployed = 2;
 
-    // Make the decision
+        // Get the unknown
+        System.out.println("Enter your salary: ");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
 
-    //
+        System.out.println("Enter the number of years with your current employer: ");
+        double years = scanner.nextDouble();
+
+        scanner.close();
+
+        // Make the decision
+        if (salary >= requiredSalary && years >= requiredYearsEmployed) {
+            System.out.println("Congratulations, you qualify for the loan!");
+        }
+        else {
+            System.out.println("You must earn at least $ "
+                    + requiredSalary + " to qualify for the loan!");
+        }
+    }
 }
