7ebdff4060fd7eb9a2ab719e2099d034040e1ee4
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2022-06-04 11:03:49 +0100

    used switch to check equality

--- src/chapter3/GradeMessage.java
+++ src/chapter3/GradeMessage.java
@@ -1,2 +1,25 @@
-package chapter3;public class GradeMessage {
+package chapter3;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your letter grade: ");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        scanner.close();
+
+        String message = switch (grade) {
+            case "A" -> "Excellent job!";
+            case "B" -> "Great job!";
+            case "C" -> "Good job!";
+            case "D" -> "You need to work a bit harder!";
+            case "F" -> "Uh oh!";
+            default -> "Error. Invalid grade";
+        };
+
+        System.out.println(message);
+    }
 }
