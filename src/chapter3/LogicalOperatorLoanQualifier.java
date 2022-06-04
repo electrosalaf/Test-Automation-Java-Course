a5ad871d6e09e625b574d4483a40e105e533f918
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2022-06-04 15:08:27 +0100

    using logical operators

---
+++ src/chapter3/LogicalOperatorLoanQualifier.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+public class LogicalOperatorLoanQualifier {
+
+    // Initiate what you know
+    int requiredSalary = 3000;
+    int requiredYearsEmployed = 2;
+
+    // Get the unknown
+
+    // Make the decision
+
+    //
+}
