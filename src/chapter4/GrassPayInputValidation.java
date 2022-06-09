91993584281a9321eb9a672d950bb9fd8fcb67ee
Ibrahim Lawal <lawalibrahimayodeji@gmail.com>
2022-06-09 08:11:36 +0100

    checking

--- src/chapter4/GrassPayInputValidation.java
+++ src/chapter4/GrassPayInputValidation.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * WHILE LOOP:
  * Each store employee makes $15 an hour. Write a program that allows the employee
  * to enter the number of hours worked for the week. Do not allow overtime.
+ * //
  */
 public class GrassPayInputValidation {
 
