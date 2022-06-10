4e576ed8ddc3515459248ed92900522e15732d29
Ibrahim Lawal <lawalibrahimayodeji@gmail.com>
2022-06-10 20:06:39 +0100

    implemented phone bill calculator

---
+++ src/chapter5/PhoneBillCalculation.java
@@ -0,0 +1,44 @@
+package chapter5;
+
+import java.util.Scanner;
+
+public class PhoneBillCalculation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter base cost of the plan:");
+        double baseCost = scanner.nextDouble();
+
+        System.out.println("Enter overage minutes:");
+        double overageMinutes = scanner.nextDouble();
+
+        double overageCharge = calculateOverages(overageMinutes);
+
+        double tax = calculateTax(baseCost + overageCharge);
+
+        calculateAndPrintBill(baseCost, overageCharge, tax);
+    }
+
+    public static double calculateOverages(double extraTime) {
+        double rate = 0.25;
+        double overage = rate * extraTime;
+        return overage;
+    }
+
+    private static double calculateTax(double subtotal) {
+        double rate = 0.15;
+        double tax = rate * subtotal;
+        return tax;
+    }
+
+    private static void calculateAndPrintBill(double base, double overage, double tax) {
+        double finalTotal = base + overage + tax;
+        System.out.println("Phone Bill Statement:");
+        System.out.println("Plan: $" + base);
+        System.out.println("Overage: $" + overage);
+        System.out.println("Tax: $" + String.format("%.2f", tax));
+        System.out.println("Total: $" + String.format("%.2f", finalTotal));
+    }
+}
