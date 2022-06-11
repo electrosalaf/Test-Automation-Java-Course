265e7ac83ec18e26a0ea9c5e46463e670ff64dcc
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2022-06-11 18:22:27 +0100

    using classes and methods

--- src/chapter6/Rectangle.java
+++ src/chapter6/Rectangle.java
@@ -1,4 +1,23 @@
 package chapter6;
 
 public class Rectangle {
+
+    double length;
+    double width;
+
+    double getLength() {
+        return length;
+    }
+
+    void setLength() {
+        this.length = length;
+    }
+
+    double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    double calculateArea() {
+        return length * width;
+    }
 }
